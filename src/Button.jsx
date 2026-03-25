fc233d60be630844760e84447e7b5e6daecc1aec
Kurtis Pivert <kurtis.pivert@gmail.com>
2026-03-25 19:44:30 -0400

    Start to add icons

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,42 @@
 // import { useState } from "react";
 
+// const Button = (props) => {
+//   return (
+//     <button
+//       onClick={() =>
+//         props.setPokemonData(
+//           props.pokemons.filter((d) => d.type === props.group),
+//         )
+//       }>
+//       {props.group}
+//     </button>
+//   );
+// };
+
+// export default Button;
+
+import fireIcon from "./assets/fire.svg";
+import waterIcon from "./assets/water.svg";
+import grassIcon from "./assets/grass.svg";
+import electricIcon from "./assets/electric.svg";
+import psychicIcon from "./assets/psychic.svg";
+import normalIcon from "./assets/normal.svg";
+import ghostIcon from "./assets/ghost.svg";
+import fightingIcon from "./assets/fighting.svg";
+import rockIcon from "./assets/rock.svg";
+
+const TYPE_ICONS = {
+  Fire: fireIcon,
+  Water: waterIcon,
+  Grass: grassIcon,
+  Electric: electricIcon,
+  Psychic: psychicIcon,
+  Normal: normalIcon,
+  Ghost: ghostIcon,
+  Fighting: fightingIcon,
+  Rock: rockIcon,
+};
+
 const Button = (props) => {
   return (
     <button
@@ -8,6 +45,7 @@ const Button = (props) => {
           props.pokemons.filter((d) => d.type === props.group),
         )
       }>
+      <img src={TYPE_ICONS[props.group]} alt="" width={16} height={16} />
       {props.group}
     </button>
   );
